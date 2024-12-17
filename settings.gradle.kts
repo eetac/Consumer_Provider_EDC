1aa5e491478beec6453b577557e782e97b0e93d1
Itziar <itziarmensaminguito@Itziars-Mac.local>
2024-12-17 08:14:24 +0100

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (c) 2022 Fraunhofer Institute for Software and Systems Engineering
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Fraunhofer Institute for Software and Systems Engineering - initial API and implementation
+ *
+ */
+
+rootProject.name = "provider_consumer"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+include(":provider-connector")
+project(":provider-connector").projectDir = file("provider/connector")
+
+include(":consumer-connector")
+project(":consumer-connector").projectDir = file("consumer/connector")
